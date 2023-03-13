315dabe235fdfd0947224866392c8faf1384bad1
Ashley Ngo <91097282+kryptokhick@users.noreply.github.com>
2023-03-13 03:55:01 -0400

    Add files via upload

---
+++ lib/productList.ts
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: "rubik-cube",
+    name: "Rubik's Cube",
+    description: "A cool Rubik's Cube that gives you superpowers",
+    photo: "/rubik-cube.jpg",
+    priceUsd: 2,
+  },
+  {
+    id: "keyboard",
+    name: "Mechanical Keyboard",
+    description: "An awesome keyboard with pleasing typing sound",
+    photo: "/keyboard.webp",
+    priceUsd: 5,
+  },
+  {
+    id: "matcha-cake",
+    name: "Matcha Cake",
+    description: "Delicious cake to accompany you into the night",
+    photo: "/matcha-cake.webp",
+    priceUsd: 1,
+  },
+];
+
+export default products;
